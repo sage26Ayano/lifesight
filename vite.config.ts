3cd44a665ca2fe1ce4eb6d57738ab63cab5b0842
aaris <aaris@moresandtechnologies.com>
2025-11-22 11:57:22 +0530

    fix/local running and production not running

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: mode === "production" ? "/lifesight/" : "/",
   server: {
     host: "::",
     port: 8080,
